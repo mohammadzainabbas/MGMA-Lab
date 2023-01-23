bfc61bedb7ac197040a1d52f26f699f0a2179322
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-23 17:22:05 +0100

    Updated code

--- src/lab4/src/notebooks/test_spark.py
+++ src/lab4/src/notebooks/test_spark.py
@@ -53,4 +53,57 @@ class PPO:
             activation1 = np.maximum(np.dot(obs, params['layer1']), 0)
             return np.dot(activation1, params['layer2'])
 
-        return critic_fn, {'layer1': np.random.randn(self.obs_dim, 64),
+        return critic_fn, {'layer1': np.random.randn(self.obs_dim, 64), 'layer2': np.random.randn(64, 1)}
+        
+    def _init_optimizer(self):
+        def optimizer_fn(params, grads, lr):
+            for p, g in zip(params.values(), grads.values()):
+                p -= lr * g
+            return params
+
+        return optimizer_fn
+
+    def compute_rtgs(self, batch_rews):
+        batch_rtgs = []
+        for ep_rews in reversed(batch_rews):
+            discounted_reward = 0 # The discounted reward so far
+            for rew in reversed(ep_rews):
+                discounted_reward = rew + discounted_reward * self.gamma
+                batch_rtgs.insert(0, discounted_reward)
+        return np.array(batch_rtgs)
+
+    def get_action(self, obs, action_tensor):
+        p1 = self.actor[0](self.actor[1], obs)
+        indices = np.nonzero(action_tensor)  # getting valid indices
+        p2 = p1[indices] # getting proper valid action probabilities
+        p2 = p2.flatten() # reshaping it to make it 1D
+        probs = Categorical(logits=p2) # logits un-normalized probabilities
+        a = probs.sample() # actions
+        action_id = indices[a.item()].item()  # valid action value
+        return action_id, a, probs.log_prob(a)
+
+    def rollout(self):
+        batch_obs, batch_acts, batch_log_probs, batch_rews, batch_rtgs, batch_lens = [], [], [], [], [], []
+        for t in range(self.timesteps_per_batch):
+            ep_rews = []
+            state, timestep = self.env.reset()
+            obs = flatten(timestep.observation)
+            while not timestep.last():
+                action_tensor = np.ones(self.act_dim)
+                action_id, a, log_prob = self.get_action(obs, action_tensor)
+                next_timestep = self.env.step(action_id)
+                next_obs = flatten(next_timestep.observation)
+                rew = next_timestep.reward
+                ep_rews.append(rew)
+                state, timestep = next_timestep
+                obs = next_obs
+                t += 1
+                #collect data
+                batch_obs.append(obs)
+                batch_acts.append(action_id)
+                batch_log_probs.append(log_prob)
+                batch_rews.append(rew)
+                batch_lens.append(t)
+            batch_rtgs.append(self.compute_rtgs(ep_rews))
+        #update
+        self.update(batch_obs,batch_acts,batch_log_probs,batch_rtgs,batch_lens)
